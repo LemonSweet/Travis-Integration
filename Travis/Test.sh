e7db0db6891f9bfa3c37c6c3d849a5520bcf2f02
AynurGaliev <aynur.galiev@flatstack.com>
2016-05-22 11:34:43 +0300

    Log commit

--- Travis/Test.sh
+++ Travis/Test.sh
@@ -8,36 +8,39 @@
 
 #!/bin/sh
 
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-echo "Testing on a branch other than master. No deployment will be done."
-exit 0
-fi
+echo "Script executed from: ${PWD}"
+ls
 
-# config github
-git config --global user.name "Travis CI"
-git config --global user.email travisci@aynurgaliev.com
-git remote add travisci https://$GITHUB_TOKEN@github.com/LemonSweet/Travis-Integration
-# if we have more than one worker we should pull before pushing
-git checkout $TRAVIS_BRANCH
-git pull travisci $TRAVIS_BRANCH
-
-echo ""
-echo "********************************"
-echo "* Change build number *"
-echo "********************************"
-echo ""
-
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $TRAVIS_BUILD_NUMBER" "$PWD/Travis/Info.plist"
-echo "New build number - $TRAVIS_BUILD_NUMBER"
-
-echo ""
-echo "********************************"
-echo "* Commit and push *"
-echo "********************************"
-echo ""
-
-git add $PWD/Travis/Info.plist
-git commit -m "Increase build number to $TRAVIS_BUILD_NUMBER by Travis CI [ci skip]"
-git push travisci $TRAVIS_BRANCH:$TRAVIS_BRANCH
+#if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+#echo "Testing on a branch other than master. No deployment will be done."
+#exit 0
+#fi
+#
+## config github
+#git config --global user.name "Travis CI"
+#git config --global user.email travisci@aynurgaliev.com
+#git remote add travisci https://$GITHUB_TOKEN@github.com/LemonSweet/Travis-Integration
+## if we have more than one worker we should pull before pushing
+#git checkout $TRAVIS_BRANCH
+#git pull travisci $TRAVIS_BRANCH
+#
+#echo ""
+#echo "********************************"
+#echo "* Change build number *"
+#echo "********************************"
+#echo ""
+#
+#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $TRAVIS_BUILD_NUMBER" "$PWD/Travis/Info.plist"
+#echo "New build number - $TRAVIS_BUILD_NUMBER"
+#
+#echo ""
+#echo "********************************"
+#echo "* Commit and push *"
+#echo "********************************"
+#echo ""
+#
+#git add $PWD/Travis/Info.plist
+#git commit -m "Increase build number to $TRAVIS_BUILD_NUMBER by Travis CI [ci skip]"
+#git push travisci $TRAVIS_BRANCH:$TRAVIS_BRANCH
 
 echo "Build success!"
